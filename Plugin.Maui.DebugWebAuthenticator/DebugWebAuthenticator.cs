d2c81b5e8471bcb4551bb90b56d56211ee4d4d26
Matt Goldman <matt.goldman@outlook.com>
2026-03-18 07:43:31 +1100

    Refactor DebugWebAuthenticator to support usage options and add WebAuthenticator implementation

--- Plugin.Maui.DebugWebAuthenticator/DebugWebAuthenticator.cs
+++ Plugin.Maui.DebugWebAuthenticator/DebugWebAuthenticator.cs
@@ -1,52 +1,24 @@
 ﻿namespace Plugin.Maui.DebugWebAuthenticator;
 
-internal class DebugWebAuthenticator : IWebAuthenticator
+public static class DebugWebAuthenticator
 {
-    public Task<WebAuthenticatorResult> AuthenticateAsync(WebAuthenticatorOptions webAuthenticatorOptions) => AuthenticateAsync(webAuthenticatorOptions, CancellationToken.None);
+    public static IWebAuthenticator Default { get; } = Microsoft.Maui.Authentication.WebAuthenticator.Default;
+    public static IWebAuthenticator Debug { get; } = new WebAuthenticator();
 
-    public async Task<WebAuthenticatorResult> AuthenticateAsync(WebAuthenticatorOptions webAuthenticatorOptions, CancellationToken cancellationToken)
+    private static IWebAuthenticator _current;
+
+    static DebugWebAuthenticator()
     {
-        // check if URL and callback URL are not null
-        if (webAuthenticatorOptions.Url == null || webAuthenticatorOptions.CallbackUrl == null)
-        {
-            return new WebAuthenticatorResult();
-        }
-
-        var tcs = new TaskCompletionSource<WebAuthenticatorResult>();
-        var dismissed = false;
-
-        var webView = new WebView
-        {
-            Source = new UrlWebViewSource { Url = webAuthenticatorOptions.Url.ToString() }
-        };
-
-        var page = new ContentPage { Content = webView };
-
-        webView.Navigating += (_, e) =>
-        {
-            if (e.Url.StartsWith(webAuthenticatorOptions.CallbackUrl.OriginalString, StringComparison.OrdinalIgnoreCase))
-            {
-                e.Cancel = true;
-                tcs.TrySetResult(new WebAuthenticatorResult(new Uri(e.Url)));
-            }
-        };
-
-        page.Disappearing += (_, _) =>
-        {
-            dismissed = true;
-            tcs.TrySetResult(new WebAuthenticatorResult());
-        };
-
-        using var reg = cancellationToken.Register(() =>
-            tcs.TrySetResult(new WebAuthenticatorResult()));
-        var navigation = Application.Current!.Windows[0].Page!.Navigation;
-        await navigation.PushModalAsync(page);
-
-        var result = await tcs.Task;
-
-        if (!dismissed)
-            await navigation.PopModalAsync();
-
-        return result;
+#if DEBUG
+        _current = Debug;
+#else
+        _current = Default;
+#endif
     }
+
+    public static Task<WebAuthenticatorResult> AuthenticateAsync(WebAuthenticatorOptions webAuthenticatorOptions)
+        => _current.AuthenticateAsync(webAuthenticatorOptions);
+
+    public static Task<WebAuthenticatorResult> AuthenticateAsync(WebAuthenticatorOptions webAuthenticatorOptions, CancellationToken cancellationToken)
+        => _current.AuthenticateAsync(webAuthenticatorOptions, cancellationToken);
 }
---
+++ Plugin.Maui.DebugWebAuthenticator/MauiAppBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Plugin.Maui.DebugWebAuthenticator;
+
+public static class MauiAppBuilderExtensions
+{
+    public static MauiAppBuilder UseDebugWebAuthenticator(this MauiAppBuilder builder)
+    {
+#if DEBUG
+        builder.Services.AddSingleton<IWebAuthenticator, WebAuthenticator>();
+#else
+        builder.Services.AddSingleton<IWebAuthenticator, Microsoft.Maui.Authentication.WebAuthenticator>();
+#endif
+        return builder;
+    }
+}
